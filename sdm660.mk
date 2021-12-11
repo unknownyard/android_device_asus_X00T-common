9d063433c26454cf522b42b3fc0c5ab0d411b22d
AT WORLD <atharvsutar3102003@gmail.com>
2021-12-11 19:46:33 +0530

    sdm660-common: Build ZenParts
    Signed-off-by: sukeeratsg <sukeerat@goindi.org>

--- sdm660.mk
+++ sdm660.mk
@@ -4,9 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# GoogleCameraGo
-$(call inherit-product, packages/apps/GoogleCam/gcam.mk)
-
 # Inherit the proprietary files
 $(call inherit-product, vendor/asus/sdm660-common/sdm660-common-vendor.mk)
 
@@ -440,3 +437,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+# Zen Parts
+PRODUCT_PACKAGES += \
+    ZenParts
